bd8be1d6736cf9aaeffafd5f769dc923a2cc92d2
alanv <alanv@labkey.com>
2026-06-22 14:01:08 -0500

    LaboratoryController::InitContainerIncrementingTableAction - Use HtmlStringBuilder

--- laboratory/src/org/labkey/laboratory/LaboratoryController.java
+++ laboratory/src/org/labkey/laboratory/LaboratoryController.java
@@ -510,20 +510,20 @@ public class LaboratoryController extends SpringActionController
         @Override
         public ModelAndView getConfirmView(SetTableIncrementForm form, BindException errors) throws Exception
         {
-            StringBuilder sb = new StringBuilder();
-            sb.append("This allows you to initialize the autoincrementing column for the provided schema/query<br><br>");
-            sb.append("This is very rarely required and was created as a helper for admins with a good deal of knowledge about this module.  Under most cases these columns will be automatically populated and you will not need to worry about this.  If you are unsure about this page, please post on the LabKey help forums, which are read by the authors of this module.<br><br>");
-            sb.append("<table style='border-collapse: collapse;'>");
+            HtmlStringBuilder sb = HtmlStringBuilder.of();
+            sb.unsafeAppend("This allows you to initialize the autoincrementing column for the provided schema/query<br><br>");
+            sb.unsafeAppend("This is very rarely required and was created as a helper for admins with a good deal of knowledge about this module.  Under most cases these columns will be automatically populated and you will not need to worry about this.  If you are unsure about this page, please post on the LabKey help forums, which are read by the authors of this module.<br><br>");
+            sb.unsafeAppend("<table style='border-collapse: collapse;'>");
 
             String schema = form.getSchemaName() == null ? "" : form.getSchemaName();
-            sb.append("<tr><td>Schema:</td><td><input name=\"schema\" value=\"" + schema + "\"></td></tr>");
+            sb.unsafeAppend("<tr><td>Schema:</td><td><input name=\"schema\" value=\"").append(schema).unsafeAppend("\"></td></tr>");
 
             String query = form.getQueryName() == null ? "" : form.getQueryName();
-            sb.append("<tr><td>Query:</td><td><input name=\"query\" value=\"" + query + "\"></td></tr>");
+            sb.unsafeAppend("<tr><td>Query:</td><td><input name=\"query\" value=\"").append(query).unsafeAppend("\"></td></tr>");
 
-            sb.append("</table><br>Do you want to continue?");
+            sb.unsafeAppend("</table><br>Do you want to continue?");
 
-            return new HtmlView(sb.toString());
+            return new HtmlView(sb.getHtmlString());
         }
 
 
